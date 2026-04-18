62e4e69092dd1c438edc8baaeb7057194707585a
deokbin <deokbin@naver.com>
2026-04-18 23:40:02 +0900

    feat(backend): sql engine 추가 및 post api 추가

--- api/main.py
+++ api/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel, Session
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from db import engine
+from model import Link 
+
+#lifespan 함수 정의
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 def read_root():
@@ -9,3 +21,12 @@ def read_root():
 @app.get('/health')
 def health_check():
     return {"status":"ok"}
+
+
+@app.post("/link")
+def add_post(link: Link):
+    with Session(engine) as session:
+        session.add(link)
+        session.commit()
+        session.refresh(link)
+    return link
