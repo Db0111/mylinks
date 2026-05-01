9cc8096d0954ab3f9a6a1e1537d9d907ced6e79a
deokbin <deokbin@naver.com>
2026-05-02 01:27:41 +0900

    fix(backend): create-engine 타입 오류 수정

--- api/db.py
+++ api/db.py
@@ -5,5 +5,7 @@ from sqlmodel import create_engine
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL, echo=True)
